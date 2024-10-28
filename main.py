6cbaced9cecea9968f4313797b23c24688cc0e61
Agonzalez3002 <agonzalez335@horizon.csueastbay.edu>
2024-10-27 19:40:37 -0700

    Made begining of Main file

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ import header
 import functions
 
 print("Welcome to Waitlist\n")
-print("\nWhat would you like to do: \n 1) Make a Waitlist\n 2) Add a person to your Waitlist\n 3) remove a person from the Waitlist\n 4) View Waitlist\n 5) Turn Waitlist into file")
+print("\nWhat would you like to do: \n 1) Make a Waitlist\n 2) Add a person to your Waitlist\n 3) remove a person from the Waitlist\n 4) View Waitlist\n 5) Turn Waitlist into file\n 6) Exit Program")
+
+while True:
+    try:
+        choice = int(input("Enter number to do task: "))
+        if choice >= 1 and choice <= 6:
+            break
+        else:
+            print("Invalid input, please enter a number between 1 and 6.")
+
+    except ValueError:
+        print("Invalid input, please enter a number between 1 and 6.")
+
 
