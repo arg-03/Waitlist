49c50ac20dcf81e6eee007334c593f6ba42c62db
Agonzalez3002 <agonzalez335@horizon.csueastbay.edu>
2024-10-27 20:42:06 -0700

    General Idea of the format of main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import header
 import functions
 
-print("Welcome to Waitlist\n")
-print("\nWhat would you like to do: \n 1) Make a Waitlist\n 2) Add a person to your Waitlist\n 3) remove a person from the Waitlist\n 4) View Waitlist\n 5) Turn Waitlist into file\n 6) Exit Program")
+print("\nWelcome to Waitlist")
+print("\nWhat would you like to do: \n \n 1) Make a Waitlist\n 2) Add a person to your Waitlist\n 3) remove a person from the Waitlist\n 4) View Waitlist\n 5) Turn Waitlist into file\n 6) Exit Program\n")
 
 while True:
     try:
@@ -15,4 +15,21 @@ while True:
     except ValueError:
         print("Invalid input, please enter a number between 1 and 6.")
 
+while choice != 6:
+    if choice == 1:
+        listName = input("Enter name of Waitlist: ")
+        print(f'{listName} Waitlist Created')
 
+    elif choice == 2:
+        print("Added Person")
+    
+    elif choice == 3:
+        print("Removed Person")
+    
+    elif choice == 4:
+        print("Waitlist: ")
+     
+    elif choice == 5:
+        print("Waitlist turned into file")
+
+    choice = int(input("Enter number to do task or enter 6 to exit: "))
