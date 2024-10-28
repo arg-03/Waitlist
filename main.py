2794215dea698eef88914f9fea60ed44fcbb8223
Agonzalez3002 <agonzalez335@horizon.csueastbay.edu>
2024-10-27 19:37:38 -0700

    Made begining of main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import header
+import functions
+
+print("Welcome to Waitlist\n")
+print("\nWhat would you like to do: \n 1) Make a Waitlist\n 2) Add a person to your Waitlist\n 3) remove a person from the Waitlist\n 4) View Waitlist\n 5) Turn Waitlist into file")
 
